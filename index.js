a2ab6dcb18e27b1bc0d49077e66f36599c108ff8
Thomas Hegeman <hegemant001@gmail.com>
2022-02-10 14:00:35 -0500

    testing says it works

--- index.js
+++ index.js
@@ -1,49 +1,65 @@
 'use strict'
 
-const arrRPS = ['rock', 'paper', 'scissors'];
-const playerSelection = 'Rock';
+const arrayComputerChoices = ['rock', 'paper', 'scissors'];
+const playerSelection = 'ROck';
 // const computerSelection = computerPlay();
 
 function computerPlay() {
   // for (let i = 1; i <= 20; i++) {
-  const randomNumber = Math.floor(Math.random() * 3);
+  const randomChoice = Math.floor(Math.random() * 3);
   // console.log(randomNumber);
   // }
-  return arrRPS[randomNumber];
+  return arrayComputerChoices[randomChoice];
 }
 
 // console.log(computerPlay());
 
-function roundWinner(roundInputs) {
+function roundWinner(plSelec, computerSelection) {
+  const roundInputs = `${plSelec}${computerSelection}`;
 
   if (
-    roundInputs === 'rockscissors' ||
-    roundInputs === 'paperrock' ||
-    roundInputs === 'scissorspaper'
+    roundInputs === 'rockscissors'
+    || roundInputs === 'paperrock'
+    || roundInputs === 'scissorspaper'
   ) {
-    return 'Player';
+
+    return `Player won! ${plSelec} beats ${computerSelection}!`;
+
   } else if (
-    roundInputs === 'rockpaper' ||
-    roundInputs === 'scissorsrock' ||
-    roundInputs === 'paperscissors'
+    roundInputs === 'rockpaper'
+    || roundInputs === 'scissorsrock'
+    || roundInputs === 'paperscissors'
   ) {
-    return 'Computer';
+
+    return `Computer won! ${computerSelection} beats ${plSelec}!`;;
+
+  } else if (
+    roundInputs === 'rockrock'
+    || roundInputs === 'paperpaper'
+    || roundInputs === 'scissorsscissors'
+  ) {
+
+    return 'Tie, play on!';
+
   }
 
-  return 'Tie';
+  return null;
 
 }
 
 function playRound(playerSelection) {
   const plSelec = playerSelection.toLowerCase();
 
-  if (plSelec === 'rock' || plSelec === 'paper' || plSelec === 'scissors') {
+  if (
+    plSelec === 'rock'
+    || plSelec === 'paper'
+    || plSelec === 'scissors'
+  ) {
     // find winner
     const computerSelection = computerPlay();
-    const roundInputs = `${plSelec}${computerSelection}`;
 
     // Player or Computer or Tie
-    const whoWon = roundWinner(roundInputs);
+    const whoWon = roundWinner(plSelec, computerSelection);
 
     // console.log(playerSelection);
     // console.log(plSelec);
@@ -51,10 +67,9 @@ function playRound(playerSelection) {
 
     return whoWon;
 
-  } else {
-    // invalid input from player
-    return 'invalid input from player';
   }
+
+  return null;
 
 }
 
@@ -64,21 +79,46 @@ function playRound(playerSelection) {
 function game(rounds = 1) {
   // round winner
   const numberOfRounds = rounds;
-  const roundWin = playRound(playerSelection);
-  const letterCheck = roundWin.charAt(0);
-  console.log(roundWin, letterCheck, numberOfRounds);
+  let playerScore = 0;
+  let computerScore = 0;
+  let winner = '';
 
   for (let i = 1; i <= rounds; i++) {
+    const playerSelection = prompt('Enter rock, paper or scissors.');
+    const roundWin = playRound(playerSelection);
+    const tieCheck = roundWin.charAt(0);
     // number of rounds played
+    if (tieCheck === 'P') {
+      // player won round
+      ++playerScore;
+
+    } else if (tieCheck === 'C') {
+      // computer won round
+      ++computerScore;
+
+    } else {
+      // tie for round
+      --i;
+
+    }
+
+    console.log(roundWin, tieCheck, numberOfRounds, computerScore, playerScore);
+
+  } // end for
+
+  if (playerScore > computerScore) {
+
+    winner = 'Player';
+
+  } else {
+
+    winner = 'Computer';
+
   }
 
-  if (letterCheck === 'P') {
-    // player won round
-  } else if (letterCheck === 'C') {
-    // computer won round
-  } else {
-    // tie for round
-  }
+  console.log(winner);
+
 }
 
-game();
+// initiate game for five rounds
+game(5);
